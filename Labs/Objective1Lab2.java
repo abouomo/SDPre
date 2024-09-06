2218705ea45bc557108b7fe3b55ed5d7e9e5e18d
Abner Bouomo <abnerbouomo@gmail.com>
2024-09-06 19:42:50 -0400

    complete objective1lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,10 @@
+public class Objective1Lab2 {
+  public static void main(String[] args) {
+    System.out.print("----------------");
+    System.out.print("| Grocery List   |");
+    System.out.print("| 1. Milk        |");
+    System.out.print("| 2. Eggs        |");
+    System.out.print("| 3. Bread       |");
+    System.out.print("----------------");
+  }
+}
